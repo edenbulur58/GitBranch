29e1476a75dcdba2b010edbdd02e9402723a8c15
hamzatechproed <66805749+hamzatechproed@users.noreply.github.com>
2020-09-30 19:38:54 +0300

    Update BranchDeneme.java

--- src/BranchDeneme.java
+++ src/BranchDeneme.java
@@ -3,13 +3,7 @@ public class BranchDeneme {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Feature branch 1. satir");
-		System.out.println("Feature branch 2. satir");
-		System.out.println("Master branch 3. satir - githubta yaptigimiz degisiklik");
-		System.out.println("Ben suan feature branch uzerinde calisiyorum");
-		System.out.println("Feature branch uzerinde calisabilirim, master branch uzerinde calismam yasak");
-		System.out.println("Tum degisiklikleri her zaman feature branch uzerinde yapabilirim.");
-		System.out.println("Yeni bir satir.");
+		// Selamlar Dunya !
 		
 	}
 	
