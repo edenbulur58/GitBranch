c22c0101fba17321697a4c503f3595fa86c21933
hamzatechproed <66805749+hamzatechproed@users.noreply.github.com>
2020-09-30 19:15:46 +0300

    Update BranchDeneme.java

--- src/BranchDeneme.java
+++ src/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 		
 		System.out.println("Feature branch 1. satir");
 		System.out.println("Feature branch 2. satir");
+		System.out.println("Master branch 3. satir - githubta yaptigimiz degisiklik");
 		
 	}
 	
